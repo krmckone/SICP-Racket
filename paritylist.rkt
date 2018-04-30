e8593046db7973f4f2d6b0612a35bd0e85597e0a
Kaleb McKone <kmckone1825@gmail.com>
2018-04-30 08:45:51 -0500

    Update paritylist.rkt

--- paritylist.rkt
+++ paritylist.rkt
@@ -6,9 +6,7 @@
           ((and (even? x) (even? (car y-list))) (parity-helper x (cdr y-list) (append new-list (list(car y-list)))))
           ((and (odd? x) (odd? (car y-list))) (parity-helper x (cdr y-list) (append new-list (list(car y-list)))))
           (else (parity-helper x (cdr y-list) new-list))))
-  (if (even? x)
-      (parity-helper x y (list x))
-      (parity-helper x y (list x))))
+  (parity-helper x y (list x)))
 
 (same-parity 1 2 3 4 5 6 7)
 (same-parity 2 3 4 5 6 7)
