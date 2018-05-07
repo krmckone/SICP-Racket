5bfcc92b0bfbccd7879ced4d38f2b2d37d48a10b
Kaleb McKone <kmckone1825@gmail.com>
2018-05-07 11:47:33 -0500

    Add files via upload

--- treemaps.rkt
+++ treemaps.rkt
@@ -13,6 +13,18 @@
              (* sub-tree sub-tree)))
          tree))
 
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+(tree-map (lambda (x) (* x x)) 
+(list 1
+(list 2 (list 3 4) 5)
+(list 6 7)))
+        
 (square-tree
 (list 1
 (list 2 (list 3 4) 5)
